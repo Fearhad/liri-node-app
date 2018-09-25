b90f961d9400833bffcded61753c1675c0f61fa4
Allan Hartigan <allan.hartigan@gmail.com>
2018-09-25 17:09:16 -0400

    Finished Movie This Functionality

--- liri.js
+++ liri.js
@@ -19,7 +19,7 @@ switch (action) {
         break;
 
     case "movie-this":
-
+        searchOmdbAPI(value);
         break;
 
     case "do-what-it-says":
@@ -53,7 +53,7 @@ function searchSpotifyAPI(songName) {
     spotify.search({ type: 'track', query: songName })
   .then(function(response) {
     var songData = ""
-    songData += "===================================\n" 
+    songData += createLine; 
     songData += "Song: " + response.tracks.items[0].name + "\n";
     songData += "Artist: " + response.tracks.items[0].artists.map(artist => artist.name).join(", ") + "\n";
     songData += "URL: " + response.tracks.items[0].album.external_urls.spotify + "\n";
@@ -62,5 +62,46 @@ function searchSpotifyAPI(songName) {
   })
   .catch(function(err) {
     console.log(err);
+    spotify.search({ type: 'track', query: 'The Sign' })
+  .then(function(response) {
+    var songData = ""
+    songData += createLine; 
+    songData += "Song: " + response.tracks.items[0].name + "\n";
+    songData += "Artist: " + response.tracks.items[0].artists.map(artist => artist.name).join(", ") + "\n";
+    songData += "URL: " + response.tracks.items[0].album.external_urls.spotify + "\n";
+    songData += "Album: " + response.tracks.items[0].album.name;
+    console.log(songData)
+  })
   });
+}
+
+function searchOmdbAPI(movieName) {
+    request("http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy", function(error, response, body) {
+
+  // If the request is successful (i.e. if the response status code is 200)
+  if (!error && response.statusCode === 200) {
+    console.log(createLine);
+    console.log("Title: " + JSON.parse(body).Title);
+    console.log("Year Released: " + JSON.parse(body).Year);
+    console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+    console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].value);
+    console.log("Country: " + JSON.parse(body).Country);
+    console.log("Language: " + JSON.parse(body).Language);
+    console.log("Plot Summary: " + JSON.parse(body).Plot);
+    console.log("Actors: " + JSON.parse(body).Actors);
+  } else {
+      movieName = "Nobody's Fool"
+    request("http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy", function(error, response, body) {
+        console.log(createLine);
+    console.log("Title: " + JSON.parse(body).Title);
+    console.log("Year Released: " + JSON.parse(body).Year);
+    console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+    console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].value);
+    console.log("Country: " + JSON.parse(body).Country);
+    console.log("Language: " + JSON.parse(body).Language);
+    console.log("Plot Summary: " + JSON.parse(body).Plot);
+    console.log("Actors: " + JSON.parse(body).Actors);
+  })
+}
+});
 }
